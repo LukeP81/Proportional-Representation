713191733a3acb29bed6e2b0abf6b23daeb19f65
lp551 <lukepeart81@gmail.com>
2024-01-10 14:22:31 +0000

    linting

--- tests/test_election_data/test_database.py
+++ tests/test_election_data/test_database.py
@@ -14,6 +14,17 @@ import election_data
 
 @contextmanager
 def mock_connection(dummy_parameter):
+    """
+    Mock connection context manager for SQLite database to replace the
+    connection method (_connect_to_database)
+
+    Parameters:
+        dummy_parameter (Any): Only present to have the same signature.
+
+    Yields:
+        sqlite3.Cursor: A mock cursor for the database connection.
+    """
+
     _ = dummy_parameter  # to avoid flagging linters
     conn = sqlite3.connect(":memory:")
     cursor = conn.cursor()
@@ -38,6 +49,14 @@ def mock_connection(dummy_parameter):
 
 @patch('sqlite3.connect')
 def test_connect_to_database(mock_connect):
+    """
+    Test the connection to the database.
+
+    This test ensures that the DatabaseElectionData class establishes a connection
+    to the database and returns the correct cursor when _connect_to_database is
+    called. It also verifies that the connection and cursor are properly closed.
+    """
+
     mock_conn = MagicMock()
     mock_cursor = MagicMock()
     mock_conn.cursor.return_value = mock_cursor
@@ -53,6 +72,13 @@ def test_connect_to_database(mock_connect):
 
 
 def test_get_elections():
+    """
+    Test the retrieval of election names from the database.
+
+    This test ensures that the get_elections method of the DatabaseElectionData
+    class retrieves the correct list of election names from the database.
+    """
+
     expected_elections = ["election1", "election2"]
 
     with patch.object(target=election_data.DatabaseElectionData,
@@ -68,6 +94,14 @@ def test_get_elections():
                          [("election1", ['Region1', 'Region2']),
                           ("election2", ['Region3', 'Region2'])])
 def test_get_regions(election, expected_regions):
+    """
+    Test the retrieval of regions for a specific election from the database.
+
+    This test ensures that the get_regions method of the DatabaseElectionData
+    class retrieves the correct list of regions for a given election from the
+    database.
+    """
+
     with patch.object(target=election_data.DatabaseElectionData,
                       attribute="_connect_to_database",
                       new=mock_connection):
@@ -77,6 +111,15 @@ def test_get_regions(election, expected_regions):
 
 
 def test_get_vote_data_no_params():
+    """
+    Test the retrieval of vote data for an election without specifying additional
+    parameters.
+
+    This test ensures that the get_vote_data method of the DatabaseElectionData
+    class retrieves the correct parties and votes for a specified election without
+    additional parameters.
+    """
+
     expected_parties = ["PartyA", "PartyB"]
     expected_votes = np.array([[10, 20], [30, 40]], dtype=np.float64)
 
@@ -92,6 +135,15 @@ def test_get_vote_data_no_params():
 
 
 def test_get_vote_data_by_region_when_region_present():
+    """
+    Test the retrieval of vote data for an election and specific region when
+    the region is present.
+
+    This test ensures that the get_vote_data method of the DatabaseElectionData
+    class retrieves the correct parties and votes for a specified election and
+    region when the region is present in the database.
+    """
+
     expected_parties = ["PartyA", "PartyB"]
     expected_votes = np.array([[10, 20]], dtype=np.float64)
 
@@ -107,6 +159,15 @@ def test_get_vote_data_by_region_when_region_present():
 
 
 def test_get_vote_data_by_region_when_region_not_present():
+    """
+    Test the retrieval of vote data for an election and specific region when the
+    region is not present.
+
+    This test ensures that the get_vote_data method of the DatabaseElectionData
+    class handles the case where the specified region is not present in the
+    database, returning empty lists for parties and votes.
+    """
+
     expected_parties = []
     expected_votes = np.array([], dtype=np.float64)
 
@@ -122,6 +183,15 @@ def test_get_vote_data_by_region_when_region_not_present():
 
 
 def test_get_vote_data_ignore_other_when_other_present():
+    """
+    Test the retrieval of vote data for an election while ignoring 'other'
+    parties when 'other' is present.
+
+    This test ensures that the get_vote_data method of the DatabaseElectionData
+    class retrieves the correct parties and votes for a specified election while
+    excluding 'other' parties, when 'other' parties are present in the database.
+    """
+
     expected_parties = ["PartyC"]
     expected_votes = np.array([[50], [70]], dtype=np.float64)
 
@@ -137,6 +207,16 @@ def test_get_vote_data_ignore_other_when_other_present():
 
 
 def test_get_vote_data_ignore_other_when_other_not_present():
+    """
+    Test the retrieval of vote data for an election while ignoring 'other'
+    parties when 'other' is not present.
+
+    This test ensures that the get_vote_data method of the DatabaseElectionData
+    class retrieves the correct parties and votes for a specified election while
+    excluding 'other' parties, when 'other' parties are not present in the
+    database.
+    """
+
     expected_parties = ["PartyA", "PartyB"]
     expected_votes = np.array([[10, 20], [30, 40]], dtype=np.float64)
 
